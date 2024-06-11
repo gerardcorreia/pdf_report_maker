d022c6469490f46cb9700bf0dfdd5c6ef81d8603
Gerard Correia <gerardcorreiacv@gmail.com>
2024-06-11 08:56:49 +0100

    Initial Commit

---
+++ pdf_report_maker/pdf_report_makerApp.swift
@@ -0,0 +1,17 @@
+//
+//  pdf_report_makerApp.swift
+//  pdf_report_maker
+//
+//  Created by user259062 on 6/11/24.
+//
+
+import SwiftUI
+
+@main
+struct pdf_report_makerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
